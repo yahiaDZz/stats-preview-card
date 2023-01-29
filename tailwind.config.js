b4658b02d30ddd9944818fecfbfc4f2827005c9b
Yahia <yahyouch36@gmail.com>
2023-01-29 21:52:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        bgcolor:"#0a0c1b",
+        divcolor:"#1c1938",
+        textpurple:"#aa5cda"
+      },
+    },
+  },
+  plugins: [],
+}
